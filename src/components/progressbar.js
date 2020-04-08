7439269e694c5d771c55cb5d7096555787802c49
Omolayo Victor <vomolayo@seamfix.com>
2020-04-08 21:25:58 +0100

    Add html and styles

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -18,3 +18,28 @@ class ProgressBar extends HTMLElement {
     return ["progress"];
   }
 
+
+  connectedCallback() {
+    var template = `
+      <style>
+        .progress-bar {
+          width: 50%;
+          height: 30px;
+          background-color: #EDF2F4;
+          border-radius: 5px;
+          color: #FFF;
+        }
+        .progress-bar-inner {
+          height: 100%;
+          line-height: 30px;
+          background: #2B2D42;
+          text-align: center;
+          border-radius: 5px;
+          transition: width 0.25s;
+        }
+      </style>
+      <div class="progress-bar">
+        <div class="progress-bar-inner">${this.complete}%</div>
+      </div>
+    `;
+
