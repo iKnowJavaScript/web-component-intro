16ac513022e404baed5fa67e5925fe47b4a5c843
Omolayo Victor <vomolayo@seamfix.com>
2020-04-08 21:24:52 +0100

    Add convienience setter and getter

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -5,3 +5,11 @@ class ProgressBar extends HTMLElement {
     this.shadow = this.createShadowRoot();
     this._progress = 0;
   }
+
+  get progress() {
+    return this._progress;
+  }
+
+  set progress(value) {
+    this.setAttribute("progress", value);
+  }
