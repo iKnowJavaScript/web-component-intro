15b04e3179a6d46afb92eb25db899c6529cad729
Omolayo Victor <vomolayo@seamfix.com>
2020-04-09 03:58:18 +0100

    Added index.js for exporting components

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// require("./components/progressbar");
+// const { ProgressBar } = require("./components/progressbar");
+// import { ProgressBar } from "./components/progressbar.js";
+
+// window.customElements.define("progress-bar", ProgressBar);
